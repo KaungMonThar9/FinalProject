e4b2050bfce4b2e178ff3f60127c0ceff82ca911
KaungMonThar9 <kaungmonthar7@gmail.com>
2025-10-28 16:44:23 -0400

    Abstract Class Entity Created - Johnathan

---
+++ src/finalProject/Entity.java
@@ -0,0 +1,24 @@
+package finalProject;
+
+import java.awt.Rectangle;
+
+public abstract class Entity {
+	double x;
+	double y; 
+	int width;
+	int height; 
+	double dx;
+	double dy;
+
+	public Entity(double x, double y, int width, int height, double dx, double dy) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public abstract Rectangle getBounds();
+	public abstract void move();
+}

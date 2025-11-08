38b3065fbb62406c2d31d693c59f38794c39602e
taylor15 <taylor15@RHIT-GAM3R9Y3AH.rose-hulman.edu>
2025-11-08 14:59:22 -0500

    Justin - One minor positional adjustment

--- src/finalProject/GameOver.java
+++ src/finalProject/GameOver.java
@@ -4,6 +4,6 @@ public class GameOver extends Level {
 	public GameOver () {
 		background = "/finalProject/images/gameOver.png";
 		
-		player = new Player(225,400);
+		player = new Player(225,300);
 	}
 }

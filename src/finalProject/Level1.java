c66f8ae427addceaba48e6d9e5690c05ffe72d93
KaungMonThar9 <kaungmonthar7@gmail.com>
2025-11-12 14:31:10 -0500

    Design for level 1

--- src/finalProject/Level1.java
+++ src/finalProject/Level1.java
@@ -3,18 +3,18 @@ package finalProject;
 public class Level1 extends Level {
 	public Level1() {
 		background = "/finalProject/images/backgroundSunny.png";
-		player= new Player(30,400); 
+		player= new Player(30,600); 
         
                  
-		 int w = 130; 
-	     int h = 20;    
+		 int w = 300; 
+	     int h = 30;    
 
 	     
-	     Platform platform1 = new Platform(90, 340, w, h, 50, 300);
-	     Platform platform2 = new Platform(220,250,w,h, 300, 500);
+	     Platform platform1 = new Platform(90, 340, w, h, 50, 550);
+	     Platform platform2 = new Platform(220,250,w,h, 550, 1050);
 	     
-	     platforms.add(new Platform(50,175,w,h, 50,300)); 
-	     platforms.add(new Platform(350,175,w,h, 300, 500)); 
+	     platforms.add(new Platform(50,480,w,h, 50,550)); 
+	     platforms.add(new Platform(350,430,w,h, 550, 1050)); 
 	      
 	    enemies.add(new Enemy(platform1.getLeftBound()+50, 340 - 50, 50, 50, 2, 0, platform1));    
 	    enemies.add(new Enemy(platform2.getLeftBound()+50, 250 - 50, 50, 50, 2, 0, platform2));     

d54143656382334fc0218a96e4d07d8b62524642
medhanshg <guptama@rose-hulman.edu>
2025-11-12 14:50:23 -0500

    Updated and final Level 1

--- src/finalProject/Level1.java
+++ src/finalProject/Level1.java
@@ -10,24 +10,21 @@ public class Level1 extends Level {
 	     int h = 30;    
 
 
-	     
-	     Platform platform3= new Platform(50,175,w,h, 50,300); 
-	    
-	     Platform platform1 = new Platform(90, 340, w, h, 50, 550);
-	     Platform platform2 = new Platform(220,250,w,h, 550, 1050);
-	     
+	     	    
+	     Platform platform1 = new Platform(90, 200, w, h, 50, 550);
+	     //Platform platform3 = new Platform(500, 175,w,h, 50,1050); 
+	     Platform platform2= new Platform(350,390,w,h, 550, 1050); 
 	     platforms.add(new Platform(50,480,w,h, 50,550)); 
-	     platforms.add(new Platform(350,430,w,h, 550, 1050)); 
+	     
 	      
-	    enemies.add(new Enemy(platform1.getLeftBound()+50, 340 - 50, 50, 50, 1, 0, platform1));    
-	    enemies.add(new Enemy(platform3.getLeftBound()+50, platform3.getY() - 50, 50, 50, 1, 0, platform3));     
+	    enemies.add(new Enemy(platform1.getLeftBound()+50, platform1.getY()- 50, 50, 50, 2, 0, platform1));    
+	    enemies.add(new Enemy(platform2.getLeftBound()+50, platform2.getY() - 50, 50, 50, 2, 0, platform2));     
 
 //	  enemies.add(new Enemy(60, 390, 50, 50, 2, 0, 50, 150));     
 //      enemies.add(new Enemy(210, 310, 50, 50, 2, 0, 200, 300));
         
 	    platforms.add(platform1);  
-	    platforms.add(platform2);
-        platforms.add(platform3); 
+        platforms.add(platform2); 
 	    
 	    collectables.add(new Collectable(130, 300));  
 	    collectables.add(new Collectable(240, 210));  
